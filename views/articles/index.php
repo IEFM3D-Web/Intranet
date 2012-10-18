efa57d836e370f49a5fffd89e268c981562dcc81
Yvettepoquer <wlkom2u@gmail.com>
2012-10-18 15:00:58 +0200

    Quelques modifs visuelles
    Affichage articles
    Ajout d'images

--- views/articles/index.php
+++ views/articles/index.php
@@ -5,16 +5,20 @@ $articlesTypesList = $aControllerDatas['articlesTypesList'];
 foreach($aControllerDatas['articles'] as $iKey => $aValue) {
 ?>
 <div class="article">
-	<div class="name"><?php echo '<a href="'.BASE_URL.'/articles/view_article/'.$aValue['id'].'">'.ucfirst($aValue['titre']).'</a>'; ?></div>
-	<div class="top_art">
-		<div class="date"><?php echo date("d/m/Y", strtotime($aValue['created'])); ?></div>
-		<div class="categorie"><?php echo ucfirst($articlesTypesList[$aValue['articles_type_id']]); ?></div>
+	<div id="article_header">
+		<div class="name"><?php echo '<a href="'.BASE_URL.'/articles/view_article/'.$aValue['id'].'">'.ucfirst($aValue['titre']).'</a>'; ?></div>
+		<div class="top_art">
+			<div class="date"><?php echo date("d/m/Y", strtotime($aValue['created'])); ?></div>
+			<div class="categorie"><?php echo ucfirst($articlesTypesList[$aValue['articles_type_id']]); ?></div>
+		</div>
 	</div>
 	<div class="clear"></div>
 	<div class="contenu"><?php echo $aValue['contenu']; ?></div>
-	<div class="auteur"><?php echo ucfirst($authorList[$aValue['created_by']]); ?></div>
-	<div>
-		<?php echo '<a href="'.BASE_URL.'/articles/view_article/'.$aValue['id'].'">';?>Commentaires (<?php echo get_nb_comments($aValue['id']); ?>)</a>
+	<div id="article_footer">
+		<div class="auteur"><?php echo ucfirst($authorList[$aValue['created_by']]); ?></div>
+		<div>
+			<?php echo '<a href="'.BASE_URL.'/articles/view_article/'.$aValue['id'].'">';?>Commentaires (<?php echo get_nb_comments($aValue['id']); ?>)</a>
+		</div>
 	</div>
 	<div class="clear"></div>
 </div>
