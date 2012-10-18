1adb3d8a6a154454ff7f7dc75e1caf2b2a1de5ca
Holic <holic@gmx.fr>
2012-10-18 14:03:22 +0200

    Modification barre d'informations

--- views/elements/intranet/header.php
+++ views/elements/intranet/header.php
@@ -1,10 +1,14 @@
 <div class="header">
 	<div class="barre_haut">
-		<div class="right_header">Bienvenue <b><?php echo ucfirst(
-			$_SESSION['prenom']).' '.ucfirst($_SESSION['nom']);?></b> | 
-			<a href="<?php echo BASE_URL."/users/profil";?>" class="profil">Profil</a> |
-			<a href="<?php echo BASE_URL."/users/document";?>" class="profil">Mes documents</a> |
+		<div class="right_header">
+		<span class="message-bienvenue">
+			Bienvenue <b><?php echo ucfirst($_SESSION['prenom']).' '.ucfirst($_SESSION['nom']);?></b>
+		</span>	
+		<span class="liens-barre-infos">
+			<a href="<?php echo BASE_URL."/users/profil";?>" class="profil">Profil</a>
+			<a href="<?php echo BASE_URL."/users/document";?>" class="profil">Mes documents</a>
 			<a href="<?php echo BASE_URL."/users/logout";?>" class="logout">Déconnexion</a>
+		</span>	
 		</div>
 	</div>
 </div>
