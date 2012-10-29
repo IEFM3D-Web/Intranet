fa9d0840fd0afd5cbd0a89b8552c30b758d99358
Holic <holic@gmx.fr>
2012-10-29 06:22:32 +0100

    test

--- views/elements/intranet/header.php
+++ views/elements/intranet/header.php
@@ -5,7 +5,7 @@
 			Bienvenue <b><?php echo ucfirst($_SESSION['prenom']).' '.ucfirst($_SESSION['nom']);?></b>
 		</span>	
 		<span class="liens-barre-infos">
-			<a href="<?php echo BASE_URL."/users/profil";?>" class="profil">Profil</a>
+			<a href="<?php echo BASE_URL."/users/profil";?>" class="profil">Mon Profil</a>
 			<a href="<?php echo BASE_URL."/users/logout";?>" class="logout">Déconnexion</a>
 		</span>	
 		</div>
