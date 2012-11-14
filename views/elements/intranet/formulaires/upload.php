9da1c494197235afd0ec8345466192ea7400a097
Holic <holic@gmx.fr>
2012-11-14 22:30:13 +0100

    Maj Bug V4

--- views/elements/intranet/formulaires/upload.php
+++ views/elements/intranet/formulaires/upload.php
@@ -3,26 +3,34 @@ echo form_input('file', "Fichier", array('type' => 'file', 'errors' => $aControl
 ?>
 
 <div id="accordion">
-	<?php foreach($aControllerDatas['sections_liste'] as $k => $v){ ?>
-		<h3 style="width:564px;"><?php echo $v;?></h3>
-		<div id="section<?php echo $k; ?>">
-			<table style="width:100%;">
-				<tr>
-				<?php 
-				$cpt_eleves = 0;
-				foreach($aControllerDatas['sections_users'][$k] as $key => $value) {
-					echo '<td>';
-					echo '<input type="checkbox" value="1" name="'.$value['folder'].'" id="Input'.$value['folder'].'" />';
-					echo $value['nom'].' '.$value['prenom'];
-					echo '</td>';
-					$cpt_eleves++;
-					if ($cpt_eleves==3) { echo'<tr></tr>'; $cpt_eleves = 0; }
-				} 
-				?>
-				</tr>
-			</table>
-			<br /><br />
-			<input type="checkbox" value="<?php echo $k; ?>" class="cocheAll" /> Tout cocher
-		</div>
-	<?php } ?>   
+	<?php 
+	foreach($aControllerDatas['sections_liste'] as $k => $v){ 
+	
+		//On test si la section n'est pas vide avant de l'afficher
+		if(count($aControllerDatas['sections_users'][$k]) > 0){
+	?>
+			<h3 style="width:564px;"><?php echo $v; ?></h3>
+			<div id="section<?php echo $k; ?>">
+				<table style="width:100%;">
+					<tr>
+					<?php 
+					$cpt_eleves = 0;
+					foreach($aControllerDatas['sections_users'][$k] as $key => $value) {
+						echo '<td>';
+						echo '<input type="checkbox" value="1" name="'.$value['folder'].'" id="Input'.$value['folder'].'" />';
+						echo $value['nom'].' '.$value['prenom'];
+						echo '</td>';
+						$cpt_eleves++;
+						if ($cpt_eleves==3) { echo'<tr></tr>'; $cpt_eleves = 0; }
+					} 
+					?>
+					</tr>
+				</table>
+				<br /><br />
+				<input type="checkbox" value="<?php echo $k; ?>" class="cocheAll" /> Tout cocher
+			</div>
+	<?php 
+		}
+	} 
+	?>   
 </div>	
