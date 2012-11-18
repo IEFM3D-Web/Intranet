a32c964e85e323e82ecea9c5716829e79ea59d8e
claire1604 <cc.cariou.claire@gmail.com>
2012-11-18 15:12:23 +0100

    debut modif affichage articles

--- views/articles/index.php
+++ views/articles/index.php
@@ -9,18 +9,17 @@ foreach($aControllerDatas['articles'] as $iKey => $aValue) {
 	<div id="article_header">
 		<div class="name"><?php echo '<a href="'.BASE_URL.'/articles/view_article/'.$aValue['id'].'">'.ucfirst($aValue['titre']).'</a>'; ?></div>
 		<div class="top_art">
-			<div class="date"><?php echo date("d/m/Y", strtotime($aValue['created'])); ?></div>
-			<div class="categorie"><?php echo ucfirst($articlesTypesList[$aValue['articles_type_id']]); ?></div>
+			<div>
+				<?php echo date("d/m/Y", strtotime($aValue['created'])); ?> |
+				<?php echo '<a href="'.BASE_URL.'/articles/view_article/'.$aValue['id'].'">';?>Commentaires (<?php echo $nbComments[$aValue['id']]; ?>)</a> | par
+				<?php echo ucfirst($authorList[$aValue['created_by']]); ?> | tags
+				<?php echo ucfirst($articlesTypesList[$aValue['articles_type_id']]); ?>
+			</div>
 		</div>
 	</div>
+	<div class="separe"></div>
 	<div class="clear"></div>
 	<div class="contenu"><?php echo $aValue['chapeau']; ?></div>
-	<div id="article_footer">
-		<div class="auteur"><?php echo ucfirst($authorList[$aValue['created_by']]); ?></div>
-		<div>
-			<?php echo '<a href="'.BASE_URL.'/articles/view_article/'.$aValue['id'].'">';?>Commentaires (<?php echo $nbComments[$aValue['id']]; ?>)</a>
-		</div>
-	</div>
 	<div class="clear"></div>
 </div>
 <?php
