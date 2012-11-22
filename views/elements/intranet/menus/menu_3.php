293d427db8e964126ecb36bd8d10fb4c8ffa407b
Holic <holic@gmx.fr>
2012-11-22 17:52:40 +0100

    maj

--- views/elements/intranet/menus/menu_3.php
+++ views/elements/intranet/menus/menu_3.php
@@ -7,10 +7,4 @@
 			<li><a href="<?php echo BASE_URL."/articles/liste"?>">Gérer les articles</a></li>
 		</ul>
 	</div>
-	<a class="menuitem submenuheader" href="">Gestion des commentaires</a>
-	<div class="submenu">
-		<ul>
-			<li><a href="<?php echo BASE_URL."/commentaires"?>">Gérer les commentaires</a></li>
-		</ul>
-	</div>
 </div>
