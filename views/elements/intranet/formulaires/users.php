01a992160e71bfea6b21c7329eb5ed66f01c1f32
Holic <holic@gmx.fr>
2012-11-23 09:48:27 +0100

    modif edit user

--- views/elements/intranet/formulaires/users.php
+++ views/elements/intranet/formulaires/users.php
@@ -1,8 +1,12 @@
 <?php 
 if(isset($aControllerDatas['users'])) { $users = $aControllerDatas['users']; }else{$users = array();} 
 
+//Si l'id existe c'est qu'on est dans le cas d'une édition d'utilisateur
 if(isset($aControllerDatas['users']['id'])){
+
 	echo form_input('id', '', array('type' => 'hidden', 'values' => $aControllerDatas['users']));
+}else{
+	$newUser = true;
 }
 
 echo form_input('nom', "Nom", array('errors' => $aControllerDatas['errors'], 'values' => $users));
@@ -11,7 +15,7 @@ echo form_input('prenom', "Prénom", array('errors' => $aControllerDatas['errors
 
 echo form_input('mail', "Mail", array('errors' => $aControllerDatas['errors'], 'values' => $users));
 
-echo form_input('password', "Password", array('errors' => $aControllerDatas['errors'], 'values' => $users));
+if(isset($newUser)){echo form_input('password', "Password", array('errors' => $aControllerDatas['errors'], 'values' => $users));}
 
 echo form_input('adresse', "Adresse", array('type' => 'textarea','errors' => $aControllerDatas['errors'], 'values' => $users,
 'rows' =>10, 'cols' =>50));
